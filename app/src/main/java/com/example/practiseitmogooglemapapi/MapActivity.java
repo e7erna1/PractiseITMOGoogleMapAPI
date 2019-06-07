d6d2396bb1319512739fc4cd597f64c4ec5509e5
iCode002 <olegegoroov@yandex.ru>
2019-06-07 09:51:00 +0300

    in Action(2)

--- app/src/main/java/com/example/practiseitmogooglemapapi/MapActivity.java
+++ app/src/main/java/com/example/practiseitmogooglemapapi/MapActivity.java
@@ -2,6 +2,8 @@ package com.example.practiseitmogooglemapapi;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +12,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -23,6 +28,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import java.io.IOException;
+import java.util.List;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -31,16 +38,65 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
   private static final int acessCode = 1234;
   private static GoogleMap gMap;
   private static FusedLocationProviderClient fusedLocationProviderClient;
+  public static Double myLatitude = null;
+  public static Double myLongitude = null;
+  public static LatLng myLatlng = null;
+
+  Button markBt;
+  Button geoLocationBt;
+  Button satView;
+  Button clear;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_map);
     getLocationAcess();
+
+//    markBt = (Button) findViewById(R.id.btMark);
+//    markBt.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        LatLng myLocation = new LatLng(myLatitude, myLongitude);
+//        gMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+//      }
+//    });
+
+//    geoLocationBt = (Button) findViewById(R.id.btSearch);
+//    geoLocationBt.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        EditText searchText = (EditText) findViewById(R.id.etLocationEntry);
+//        String search = searchText.getText().toString();
+//        if (search != null && !search.equals("")) {
+//          List<Address> addressList = null;
+//          Geocoder geocoder = new Geocoder(MapActivity.this);
+//          try {
+//            addressList = geocoder.getFromLocationName(search, 1);
+//          } catch (IOException e) {
+//            e.printStackTrace();
+//          }
+//          Address address = addressList.get(0);
+//          LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+//          gMap.addMarker(new MarkerOptions().position(latLng).title("from geocoder"));
+//          gMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+//        }
+//      }
+//    });
+
+//    clear = (Button) findViewById(R.id.btClear);
+//    clear.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        gMap.clear();
+//      }
+//    });
+
   }
 
   private void initMap() {
-    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.map);
     assert supportMapFragment != null;
     supportMapFragment.getMapAsync(MapActivity.this);
   }
@@ -61,8 +117,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
       gMap.setMyLocationEnabled(true);
     }
 
+//    gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//      @Override
+//      public void onMapClick(LatLng latLng) {
+//        gMap.addMarker(new MarkerOptions().position(latLng).title("from onMapClick"));
+//        gMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+//        Log.d(TAG, new MarkerOptions().position(latLng).toString());
+//      }
+//    });
 
-    googleMap.addMarker(new MarkerOptions().position(new LatLng(37.4220, -122.0840)).title("HW!"));
+//    markBt = (Button) findViewById(R.id.btMark);
+//    markBt.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        LatLng myLocation = new LatLng(myLatitude, myLongitude);
+//        gMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+//      }
+//    });
+
+    gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+      @Override
+      public void onMapClick(LatLng latLng) {
+         myLatlng = latLng;
+        Log.d(TAG, new MarkerOptions().position(latLng).toString());
+      }
+    });
+
+    markBt = (Button) findViewById(R.id.btMark);
+    markBt.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        LatLng myLocation = myLatlng;
+        gMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+      }
+    });
+
+
   }
 
   private void getLocationAcess() {
